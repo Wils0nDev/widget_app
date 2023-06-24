10c93480486be83bc9eb5f14bf1a4f7331b31b4d
wilsonvasquez <ewilsonvc.dev@gmail.com>
2023-06-24 11:52:29 -0500

    screen creadas y barril actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,11 @@
+
+
+export 'package:flutter/material.dart';
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_scree.dart';
-
+export 'package:widgets_app/presentation/screens/inifite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls.dart';
